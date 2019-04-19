053b236683db5acd1f76bb67ad585d704b760196
bhavin nakrani <bhavin.it8488@gmail.com>
2019-04-19 18:54:37 +0530

    working in email extractor

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validation;
 use Symfony\Component\Validator\Constraints\Url as UrlConstraint;
+use Goutte\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
 class HomeController extends AbstractController
 {
@@ -38,11 +40,35 @@ class HomeController extends AbstractController
             foreach ($violations as $violation) {
                 echo $violation->getMessage().'<br>';
             }
+
+            $this->redirectToRoute('homepage');
         }
         /* Validation End */
 
-        
-        echo $url;
-        exit;
+        $client = new Client();
+        $crawler = $client->request('GET', $url);
+        $links_count = $crawler->filter('a')->count();
+        $all_links = []; $i=0;
+        if($links_count > 0){
+            $links = $crawler->filter('a')->links();
+
+            foreach ($links as $link) {
+
+                //if ($i > 10) { break;}
+                $all_links[] = $link->getURI();
+                $i++;
+            }
+
+            $all_links = array_unique($all_links);
+        }
+
+        return $this->render('search.html.twig', ['all_links' => $all_links]);
+    }
+
+    private function extractEmails ($string){
+
+        $pattern = '/[a-z0-9_\-\+\.]+@[a-z0-9\-]+\.([a-z]{2,4})(?:\.[a-z]{2})?/i';
+        preg_match_all($pattern, $string, $matches);
+        print_r($matches[0]);
     }
 }
